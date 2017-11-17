debcc49a91556763bd8e193457067671a4d4b389
Michał Kurzydłowski <michalk@mimuw.edu.pl>
2017-11-17 13:44:25 +0100

    Remove EchoValidator dependency on Application

--- src/main/java/eu/erasmuswithoutpaper/registry/echovalidator/EchoValidator.java
+++ src/main/java/eu/erasmuswithoutpaper/registry/echovalidator/EchoValidator.java
@@ -108,12 +108,8 @@ public class EchoValidator {
     /* Generate the IDs of the covered HEIs. */
 
     this.myCoveredHeiIDs = new ArrayList<>();
-    if (Application.getRootUrl().equals("https://registry.erasmuswithoutpaper.eu")) {
-      // We will never introduce artificial HEIs in the official registry.
-    } else {
-      for (int i = 1; i <= 2; i++) {
-        this.myCoveredHeiIDs.add("hei0" + i + ".developers.erasmuswithoutpaper.eu");
-      }
+    for (int i = 1; i <= 2; i++) {
+      this.myCoveredHeiIDs.add("hei0" + i + ".developers.erasmuswithoutpaper.eu");
     }
   }
 
